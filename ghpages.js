06abd1dfa0ede74f5cf2c3b49f9d031249356065
msx47 <mohitsrivastava67@gmail.com>
2021-12-13 00:14:58 +0530

    added ghpages

---
+++ ghpages.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const ghpages = require("gh-pages");
+const { exec } = require("child_process");
+
+exec("yarn build", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+
+  readWriteSync();
+
+  ghpages.publish("dist", function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success");
+    }
+  });
+});
+function readWriteSync() {
+  try {
+    let data = fs.readFileSync("dist/index.html", "utf-8");
+
+    let newValue = data.replace(/="\//gm, '="');
+
+    fs.writeFileSync("dist/index.html", newValue, "utf-8");
+
+    console.log("Updated index.html");
+  } catch (e) {
+    console.log(e);
+  }
+}
